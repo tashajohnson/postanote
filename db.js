eb313013281195cd5f1598d6ace4eb40cdbe1a65
tashajohnson <tasha.hubner@gmail.com>
2016-03-24 20:00:16 -0600

    radio buttons for color

--- db.js
+++ db.js
@@ -5,6 +5,8 @@ var Postnote = new Schema({
   title: {type: String},
   content: {type: String, required: true},
   color: {type: String},
+  top: Number,
+  left: Number,
   updated_at: Date,
 });
 
--- routes/posts.js
+++ routes/posts.js
@@ -17,6 +17,7 @@ router.post('/', function(req, res) {
   new Postnote({
     title: req.body.title,
     content: req.body.content,
+    color: req.body.color,
     updated_at: Date.now()
   }).save( function(err, posts) {
     res.redirect('/posts');
@@ -27,13 +28,16 @@ router.post('/', function(req, res) {
 // Get specific note
 
 router.get('/:id', function(req, res) {
-  Postnote.findById(req.params.id, function(err, posts) {
-    res.render('posts', {posts: posts});
+  Postnote.findById(req.params.id, function(err, post) {
+    res.render('post', {post: post});
   });
 });
 
 // Edit a note
-
+// ajax call would go to this route
+// get the data from the ajax call
+// update the database
+// card will have location data on it
 
 // Delete a note
 router.post('/:id', function(req, res) {
